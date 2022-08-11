a2f36e30a0a1272a8936448486b174eaac622cd5
SircCruz <christophercruz936@gmail.com>
2022-08-11 10:52:05 +0800

    script.js:54: Modify className value

--- script.js
+++ script.js
@@ -51,10 +51,10 @@ var timeout2;
 
 function imageSet2(){
     try{
-        document.getElementById("set2img" + temp2).className = "inactive";
-        document.getElementById("set2img" + image2).className = "active";
-        document.getElementById("set2btn" + temp2).className = "inactive";
-        document.getElementById("set2btn" + image2).className = "active";
+        document.getElementById("set2img" + temp2).className = "inactive btn-page";
+        document.getElementById("set2img" + image2).className = "active btn-page";
+        document.getElementById("set2btn" + temp2).className = "inactive btn-page";
+        document.getElementById("set2btn" + image2).className = "active btn-page";
         temp2 = image2;
         image2++;
         timeout2 = setTimeout(imageSet2, 5000);
