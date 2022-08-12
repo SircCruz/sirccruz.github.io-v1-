088f9a58c9c2cca209c22d5397abd23c283d7d70
SircCruz <christophercruz936@gmail.com>
2022-08-12 23:52:56 +0800

    script.js: Refactor script.js

--- script.js
+++ script.js
@@ -67,8 +67,6 @@ function removeImages(){
 var image1 = 0;
 var temp1 = 0;
 var timeout1;
-var isImgEnlarged = false;
-
 function imageSet1(){
     try{
         document.getElementById("set1img" + temp1).className = "inactive btn-page";
@@ -92,16 +90,6 @@ function setImg1(img){
     imageSet1();
 }
 
-function nextImg1(){
-    clearTimeout(timeout1);
-    imageSet1();
-}
-function prevImg1(){
-    clearTimeout(timeout1);
-    image1 -= 2;
-    imageSet1();
-}
-
 imageSet1();
 
 var image2 = 0;
@@ -121,23 +109,13 @@ function imageSet2(){
     }
     catch{
         image2 = 0;
-        setTimeout(imageSet2, 0);
+        imageSet2();
     }
 }
 
 function setImg2(img){
     clearTimeout(timeout2);
     image2 = img;
-    imageSet2();
-}
-
-function nextImg2(){
-    clearTimeout(timeout2);
-    imageSet2();
-}
-function prevImg2(){
-    clearTimeout(timeout2);
-    image2 -= 2;
     imageSet2();
 }
 
@@ -167,15 +145,5 @@ imageSet3();
 function setImg3(img){
     clearTimeout(timeout3);
     image3 = img;
-    imageSet3();
-}
-
-function nextImg3(){
-    clearTimeout(timeout3);
-    imageSet3();
-}
-function prevImg3(){
-    clearTimeout(timeout3);
-    image3 -= 2;
     imageSet3();
 }
