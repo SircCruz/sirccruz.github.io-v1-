d36d2f1c778366464f968c5cae3a743c2a1f87d1
SircCruz <christophercruz936@gmail.com>
2022-08-13 00:10:56 +0800

    index.html:321: Modify id value

--- script.js
+++ script.js
@@ -67,6 +67,7 @@ function removeImages(){
 var image1 = 0;
 var temp1 = 0;
 var timeout1;
+var isImgEnlarged = false;
 function imageSet1(){
     try{
         document.getElementById("set1img" + temp1).className = "inactive btn-page";
@@ -83,19 +84,24 @@ function imageSet1(){
         imageSet1();
     }
 }
-
 function setImg1(img){
     clearTimeout(timeout1);
     image1 = img;
     imageSet1();
 }
-
+function nextImg1(){
+    clearTimeout(timeout1);
+    imageSet1();
+}
+function prevImg1(){
+    clearTimeout(timeout1);
+    image1 -= 2;
+    imageSet1();
+}
 imageSet1();
-
 var image2 = 0;
 var temp2 = 0;
 var timeout2;
-
 function imageSet2(){
     try{
         document.getElementById("set2img" + temp2).className = "inactive btn-page";
@@ -109,18 +115,24 @@ function imageSet2(){
     }
     catch{
         image2 = 0;
-        imageSet2();
+        setTimeout(imageSet2, 0);
     }
 }
-
 function setImg2(img){
     clearTimeout(timeout2);
     image2 = img;
     imageSet2();
 }
-
+function nextImg2(){
+    clearTimeout(timeout2);
+    imageSet2();
+}
+function prevImg2(){
+    clearTimeout(timeout2);
+    image2 -= 2;
+    imageSet2();
+}
 imageSet2()
-
 var image3 = 0;
 var temp3 = 0;
 var timeout3;
@@ -141,9 +153,17 @@ function imageSet3(){
     }
 }
 imageSet3();
-
 function setImg3(img){
     clearTimeout(timeout3);
     image3 = img;
+    imageSet3();
+}
+function nextImg3(){
+    clearTimeout(timeout3);
+    imageSet3();
+}
+function prevImg3(){
+    clearTimeout(timeout3);
+    image3 -= 2;
     imageSet3();
 }
