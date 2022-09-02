abd5030887e9c4936bd7a26662eb3b85347f6565
SircCruz <christophercruz936@gmail.com>
2022-09-02 18:39:34 +0800

    script.js:308: Make submission asynchronous

--- script.js
+++ script.js
@@ -305,12 +305,30 @@ function prevImg3(){
         imageSet3();
     }
 }
-function Send(){
-    $.ajax({
-        type: "POST",
-        url: "https://formspree.io/f/mwkzveza",
-        success: function () {
-            alert("Thank you for your feedback!");
+
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
         }
-    });
-}
+      }).then(response => {
+        if (response.ok) {
+          alert("Thanks for your submission!");
+          form.reset()
+        } else {
+              alert("Oops! There was a problem submitting your form");
+        }
+      }).catch(error => {
+        alert("Oops! There was a problem submitting your form");
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
+    
+
+    
